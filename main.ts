a7866d16ecb3bd167a3e80e8f78ae0e0cf7ac38c
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-11-18 15:44:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-led.plotBrightness(0, 0, 255)
+let sprite = 0
+input.onButtonPressed(Button.A, function () {
+    sprite = randint(0, 100)
+    if (sprite % 7 == 0) {
+        basic.showNumber(sprite)
+        basic.showIcon(IconNames.Meh)
+    } else if (sprite % 5 == 0) {
+        basic.showNumber(sprite)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showNumber(sprite)
+        basic.showIcon(IconNames.Sad)
+    }
+})
